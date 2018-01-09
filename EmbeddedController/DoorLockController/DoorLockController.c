ed405bc3b3df8dd6ba2e0c58db33d8814fe142de
gutsavgupta <gutsavgupta@gmail.com>
2018-01-10 02:12:10 +0530

    Directory Structure

---
+++ EmbeddedController/DoorLockController/DoorLockController.c
@@ -0,0 +1,17 @@
+/*********************************************************************/
+/**** This Program contains arduino code for door lock and close  ****/
+/**** Author:                                                     ****/
+/**** Date                                                        ****/
+/**** Functionality:                                              ****/
+/*********************************************************************/
+
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+    printf("This is a test code\n");
+    return 0;
+}
